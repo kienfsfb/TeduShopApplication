041e6f6f7c35212cfed1717c4392f2ba48a6443a
kien <kien@gmail.com>
2023-10-04 23:16:58 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TeduShopApplication.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
